9cfb4fdbbed6af7d394e69e22f35844bd1ed89b5
Prm <prm@datacyber.com>
2023-07-26 23:09:28 +0800

    update Chat CRUD测通

--- src/main/java/com/anii/querydsl/dao/ChatMessageRepository.java
+++ src/main/java/com/anii/querydsl/dao/ChatMessageRepository.java
@@ -2,6 +2,11 @@ package com.anii.querydsl.dao;
 
 import com.anii.querydsl.entity.ChatMessage;
 import com.infobip.spring.data.r2dbc.QuerydslR2dbcRepository;
+import org.springframework.data.r2dbc.repository.Query;
+import reactor.core.publisher.Flux;
 
 public interface ChatMessageRepository extends QuerydslR2dbcRepository<ChatMessage, Long> {
+
+    @Query("SELECT * FROM chat_message WHERE chat_id=:chatId ORDER BY create_time DESC LIMIT :limit")
+    Flux<ChatMessage> findAllByChatIdLimit(Long chatId, Integer limit);
 }
