4f776f422c8240af95f14f7d7f0aa2f973ec6321
prm <prm@datacyber.com>
2023-08-16 15:38:45 +0800

    delete 删除criteriaUtils

--- src/main/java/com/anii/querydsl/common/utils/function/SFunction.java
+++
@@ -1,8 +0,0 @@
-package com.anii.querydsl.common.utils.function;
-
-import java.io.Serializable;
-import java.util.function.Function;
-
-@FunctionalInterface
-public interface SFunction<T, R> extends Function<T, R>, Serializable {
-}
